50d7af6a6822b3fcc749fa3f55378d304dfcbcd4
romeon728 <60239236+romeon728@users.noreply.github.com>
2025-12-26 19:40:18 -0500

    database-implementation (#3)

---
+++ src/db/add_rules.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import logging
+import json
+
+from database import LedgerlyDatabase
+
+DB = LedgerlyDatabase()
+RULES = {}
+
+def load_rules(path="rules/merchant_rules.json"):
+  """
+  Load the transactions set in path
+  """
+  with open(path, "r") as f:
+    rules = json.load(f)
+  
+  
+  logging.info(f"{len(rules)} Rules Loaded")
+  
+  global RULES
+  RULES = rules
+  
+  return 0
+
+def add_rules():
+	
+	logging.info("THIS IS TBD")
+	
+	DB.close()
+	return
+
+# ----------------------------------------------------------------
+# Main execution
+# ----------------------------------------------------------------
+if __name__ == "__main__":
+  # Configure logging
+  logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+  DB.connect()
+  load_rules()
+  add_rules()
